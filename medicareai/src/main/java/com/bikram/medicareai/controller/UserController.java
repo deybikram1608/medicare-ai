c7c2a3f9b1fe4d65b4e38d486d0297ff9e850c08
Bikram Dey <deybikram1608@gmail.com>
2026-08-01 00:18:15 +0530

    Implement user regestration api

---
+++ medicareai/src/main/java/com/bikram/medicareai/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.bikram.medicareai.controller;
+
+import com.bikram.medicareai.entity.User;
+import com.bikram.medicareai.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user){
+        return userService.registerUser(user);
+    }
+}
---
+++ medicareai/src/main/java/com/bikram/medicareai/service/UserService.java
@@ -0,0 +1,19 @@
+package com.bikram.medicareai.service;
+
+import com.bikram.medicareai.entity.User;
+import com.bikram.medicareai.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user){
+        return userRepository.save(user);
+    }
+}
